5743fe2485f8ef4aa40b59b6506f31fcc7aa7692
andrerim <andreasrim@gmail.com>
2020-01-13 20:38:08 +0100

    reformatted code

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 
-
 my_url = "https://www.nordnet.no/market/no"
 
 # open webpage
@@ -18,7 +17,6 @@ tabels = page_soup.find_all("div", {"class": "InstrumentsPanel__Padded-hx9m9d-1 
 most_traded = tabels[0]
 
 elements = most_traded.find_all("tr", {"class": "Tr__StyledTr-sc-1aap6l7-0 kdcphJ"})
-#print(len(elements))
 
 out_filename = "stock_prices_most_traded.csv"
 # norsk csv bruker ; som verdiskille og "," som komma
@@ -27,28 +25,24 @@ headers = "stock_name;change;price \n"
 f = open(out_filename, "w")
 f.write(headers)
 
-
 for element in elements:
-    #print(element)
     instrument_name = element.find("span", {"class": "Typography__StyledTypography-sc-10mju41-0 dmJcIB"})
-
 
     # includes %: instrument_change = element.find_all("td", {"class": "Td__StyledTd-sc-1r6yxrk-0 eSYZap"})
     instrument_change = element.find("span", {"class": "Development__StyledDevelopment-hnn1ri-0"})
 
-    instrument_price = element.find("td", {"class": "Td__StyledTd-sc-1r6yxrk-0 eSYZap Media__StyledDiv-sc-1dic02p-0 fxoRoi"})
+    instrument_price = element.find("td",
+                                    {"class": "Td__StyledTd-sc-1r6yxrk-0 eSYZap Media__StyledDiv-sc-1dic02p-0 fxoRoi"})
 
-    #instrument_change_negative = element.find("span", {"class": "Development__StyledDevelopment-hnn1ri-0 cuAsQS"})
+    # instrument_change_negative = element.find("span", {"class": "Development__StyledDevelopment-hnn1ri-0 cuAsQS"})
     # Positive: Development__StyledDevelopment-hnn1ri-0 kokOki
     # Negative: Development__StyledDevelopment-hnn1ri-0 cuAsQS
-    if not instrument_name == None:
+    if instrument_name is not None:
         instrument_name = instrument_name.text.replace(" ", "_")
         instrument_change = instrument_change["value"].replace(".", ",")
         instrument_price = instrument_price.text
-        #print(instrument_name, instrument_change + '%', instrument_price)
+        # print(instrument_name, instrument_change + '%', instrument_price)
         print(instrument_name, instrument_change, instrument_price)
         f.write(instrument_name + ";" + instrument_change + ";" + instrument_price + "\n")
 
-
 f.close()
-
