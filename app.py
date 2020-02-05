ce4fbcc58ab0f974bd4ac51f621c8288dbf5d285
andrerim <andreasrim@gmail.com>
2020-02-05 21:30:10 +0100

    Search for stocks to display

--- app.py
+++ app.py
@@ -1,20 +1,57 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 import pandas as pd
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
 import csv
+import os.path
+
+from scrape_historical_data import scrape_historical_data
 
 csv.register_dialect('nor', delimiter=';')
 
-df = pd.read_csv("../scraped_data/historical_dataEQNR.csv", dialect='nor')
-print(df.head())
+def draw_plot_stock(ticker):
+    filename = "../scraped_data/historical_data_" + ticker + ".csv"
+
+    if not os.path.isfile(filename):
+        instr_name = scrape_historical_data(ticker)
+
+    df = pd.read_csv(filename, dialect='nor')
 
 
-fig = go.Figure(data=[go.Scatter(x=df["Date"], y=df["Adj Close**"], name="awd")])
-#fig.add_trace(go.Figure(data=[go.Scatter(x=df["Date"], y=df["Volume"], name="awd")]))
+    try:
+        fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+        #fig = go.Figure()
+
+        fig.add_trace(go.Scatter(x=df["Date"].iloc[::-1], y=df["Adj Close**"].iloc[::-1], name="Stock price"), secondary_y=False)
+
+        fig.add_trace(go.Bar(x=df["Date"].iloc[::-1], y=df["Volume"].iloc[::-1], opacity=0.3, name="Volume"), secondary_y=True)
+        fig.update_layout(title_text=ticker.upper())
+
+        fig.update_xaxes(title_text="Date")
+        fig.update_yaxes(title_text="Price NOK", secondary_y=False)
+        fig.update_yaxes(title_text="Volume NOK", secondary_y=True)
+    except:
+        print("Error")
+
+
+
+    return fig
+
+fig = draw_plot_stock("EQNR")
+
 #fig.show()
+
+#external_stylesheets = ['./style.css']
+
 app = dash.Dash(__name__)
+
+
 
 app.layout = html.Div(children=[
     html.H1(children='Stock analyser'),
@@ -25,9 +62,30 @@ app.layout = html.Div(children=[
 
     dcc.Graph(
     id='stock-graph',
-        figure=fig,
-    )
+        figure=fig
+    ),
+
+    dcc.Input(
+        id="ticker",
+        type="text",
+        value=None,
+        placeholder="Ticker, e.g. EQNR, DNB..",
+        debounce=True
+    ),
+    html.Div(id="out-all-types")
 ])
+
+@app.callback(
+    Output("stock-graph", "figure"),
+    [Input("ticker", "value")]
+
+)
+def load_stock(val):
+    if val is not None:
+        return draw_plot_stock(val)
+    else:
+        return draw_plot_stock("EQNR")
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- scrape_historical_data.py
+++ scrape_historical_data.py
@@ -1,46 +1,51 @@
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 
-ticker = "EQNR"
 
-url_scrape = "https://finance.yahoo.com/quote/" + ticker + ".OL/history?p=" + ticker + ".OL"
+def scrape_historical_data(ticker):
 
-# open webpage
-client = urlopen(url_scrape)
+    url_scrape = "https://finance.yahoo.com/quote/" + ticker + ".OL/history?p=" + ticker + ".OL"
 
-# read page
-raw_html = client.read()
+    # open webpage
+    client = urlopen(url_scrape)
 
-# close webpage
-client.close()
+    # read page
+    raw_html = client.read()
 
-page_soup = soup(raw_html, "html.parser")
-title = page_soup.find("title").text
-#Equinor ASA (EQNR) Stock Price, Quote, History & News - Yahoo Finance
-instr_name_pos = title.find("Stock Price")
-instr_name = title[:instr_name_pos]
-print(title)
+    # close webpage
+    client.close()
 
-
-table_head = page_soup.find("thead")
-table_headers = ""
-for row in table_head:
-    for cols in row:
-        table_headers += cols.text + ";"
+    page_soup = soup(raw_html, "html.parser")
+    title = page_soup.find("title").text
+    #Equinor ASA (EQNR) Stock Price, Quote, History & News - Yahoo Finance
+    instr_name_pos_start = title.find("(") + 1
+    instr_name_pos_end = title.find(".OL")
+    instr_name = title[instr_name_pos_start:instr_name_pos_end]
+    print(instr_name)
 
 
-out_filename = "../scraped_data/historical_data_" + ticker + ".csv"
+    table_head = page_soup.find("thead")
+    table_headers = ""
+    for row in table_head:
+        for cols in row:
+            table_headers += cols.text + ";"
 
-f = open(out_filename, "w")
-f.write(table_headers + "\n")
 
-table_body = page_soup.find("tbody")
+    out_filename = "../scraped_data/historical_data_" + instr_name + ".csv"
 
-for tab in table_body:
-    data = ""
-    for span in tab:
-        data += span.text + ";"
-    f.write(data + "\n")
+    f = open(out_filename, "w")
+    f.write(table_headers + "\n")
 
-f.close()
+    table_body = page_soup.find("tbody")
+
+    for tab in table_body:
+        data = ""
+        for span in tab:
+            data += span.text + ";"
+        f.write(data + "\n")
+
+    f.close()
+
+    return instr_name
+
 
